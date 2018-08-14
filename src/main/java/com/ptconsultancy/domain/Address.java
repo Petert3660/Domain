60d9ade93db71772b129fca08abfc1f424959f79
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-08-14 08:48:45 +0100

    First Commit

---
+++ src/main/java/com/ptconsultancy/domain/Address.java
@@ -0,0 +1,104 @@
+package com.ptconsultancy.domain;
+
+import java.io.Serializable;
+
+public class Address implements Serializable {
+
+    private Long userId;
+    private String houseNameNumber;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String addressLine4;
+    private String county;
+    private String country;
+    private String postCode;
+
+    public Address() {
+    }
+
+    public Address(long userId, String houseNameNumber, String addressLine1, String addressLine2,
+        String addressLine3, String addressLine4, String county, String country, String postCode) {
+        this.userId = userId;
+        this.houseNameNumber = houseNameNumber;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.addressLine3 = addressLine3;
+        this.addressLine4 = addressLine4;
+        this.county = county;
+        this.country = country;
+        this.postCode = postCode;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getHouseNameNumber() {
+        return houseNameNumber;
+    }
+
+    public void setHouseNameNumber(String houseNameNumber) {
+        this.houseNameNumber = houseNameNumber;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getAddressLine3() {
+        return addressLine3;
+    }
+
+    public void setAddressLine3(String addressLine3) {
+        this.addressLine3 = addressLine3;
+    }
+
+    public String getAddressLine4() {
+        return addressLine4;
+    }
+
+    public void setAddressLine4(String addressLine4) {
+        this.addressLine4 = addressLine4;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+}
