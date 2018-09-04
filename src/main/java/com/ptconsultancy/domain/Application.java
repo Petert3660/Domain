50e91dc6e9ae99fac75c7bdf712c3d816924a513
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-09-04 13:41:09 +0100

    master
    Update of Application file to satisfy Spring Boot

--- src/main/java/com/ptconsultancy/domain/Application.java
+++ src/main/java/com/ptconsultancy/domain/Application.java
@@ -1,12 +1,17 @@
 package com.ptconsultancy.domain;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class);
+        new SpringApplicationBuilder(Application.class).run();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
     }
 }
