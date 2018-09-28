9dcca3055d0b1fd84df59a6c4687a52746d907c3
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-09-28 15:52:48 +0100

    master
    Adding the integer number check to Domain utilities

--- src/main/java/com/ptconsultancy/domain/utilities/CommonUtils.java
+++ src/main/java/com/ptconsultancy/domain/utilities/CommonUtils.java
@@ -15,4 +15,18 @@ public class CommonUtils {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         return time.format(formatter);
     }
+
+    public static boolean isNumberAnInteger(String input) {
+        boolean isNumInteger = true;
+        // Check that input number is an integer
+        try {
+
+            int portNum = Integer.parseInt(input);
+
+        } catch (NumberFormatException nfe) {
+            isNumInteger = false;
+        }
+        
+        return isNumInteger;
+    }
 }
