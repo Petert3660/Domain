8f16c48ce132e867c0e9b78cd9c4d8b7d55bd17c
Peter Thomson <peter.thomson@thehutgroup.com>
2018-11-07 14:23:21 +0000

    master
    Update of FreeComboBox class to set standrad dropdown select

--- src/main/java/com/ptconsultancy/domain/guicomponents/FreeComboBox.java
+++ src/main/java/com/ptconsultancy/domain/guicomponents/FreeComboBox.java
@@ -13,34 +13,34 @@ public class FreeComboBox extends JComboBox {
     }
 
     public FreeComboBox(int x, int y, int xSize, int ySize, ArrayList<String> items) {
-            this.setBounds(x, y, xSize, ySize);
-            if (items != null) {
-                for (String item : items) {
-                    this.addItem(item);
-                }
-            }
-        }
-
-        public void focusComboBox() {
-            this.grabFocus();
-        }
-
-        public String getItem(int index) {
-            return (String) this.getItemAt(index);
-        }
-
-        public String getSelectedItem() {
-            return getItem(this.getSelectedIndex());
-        }
-
-        public void clearComboBox() {
-            this.setSelectedIndex(0);
-        }
-
-        public void repopulateComboBox(ArrayList<String> items) {
-            this.removeAllItems();
+        this(x, y, xSize, ySize);
+        if (items != null) {
             for (String item : items) {
                 this.addItem(item);
+            }
         }
+    }
+
+    public void focusComboBox() {
+        this.grabFocus();
+    }
+
+    public String getItem(int index) {
+        return (String) this.getItemAt(index);
+    }
+
+    public String getSelectedItem() {
+        return getItem(this.getSelectedIndex());
+    }
+
+    public void clearComboBox() {
+        this.setSelectedIndex(0);
+    }
+
+    public void repopulateComboBox(ArrayList<String> items) {
+        this.removeAllItems();
+        for (String item : items) {
+            this.addItem(item);
+    }
     }
 }
